2182fa95bbf2af1a3a19556b2d2954f156afbeb0
wendy <1104777947@qq.com>
2017-02-16 11:59:01 +0800

    Update index.php

--- system/modules/dashboard/views/default/index.php
+++ system/modules/dashboard/views/default/index.php
@@ -26,106 +26,28 @@ use application\core\utils\Ibos;
             adjustSidebarWidth();
             window.onresize = adjustSidebarWidth;
         </script>
-        <!-- <div style="height: 100%"> -->
-        <div class="db-map" id="db_map" style="display:none;">
-            <ul class="dbm-main-list">
-                <?php foreach ( $routes as $cate => $routeA ): ?>
-                    <li class="dbm-main-item clearfix">
-                        <div class="dbm-main-item-name"><?php echo $lang[$cateConfig[$cate]['lang']]; ?></div>
-                        <ul class="dbm-sub-list">
-                            <?php foreach ( $routeA as $route => $config ): ?>
-                                <?php if ( $config['config']['isShow'] ): ?>
+<!--        --><?php //var_dump($cateConfig);die;?>
+        <div style="margin-top: 100px;">
+        <ul class="nav metismenu">
+            <?php foreach ($cateConfig as $key => $value):?>
+                <li>
+                    <a href="#"><span class="nav-label"><?php echo $lang[$value['lang']];?></span></a>
+                    <?php if (!empty($routes[$value['id']])):?>
+                        <ul class="nav nav-second-level collapse" style="height: 0px;">
+                        <?php foreach ($routes[$value['id']] as $routekey => $routevalue):?>
+                                <?php $config = $routevalue['config'];?>
+                                <?php if ($config['isShow']):?>
                                     <li>
-                                        <a href="<?php echo $config['url']; ?>" target="main" title="<?php echo $lang[$config['config']['lang']]; ?>">
-                                            <?php echo $lang[$config['config']['lang']]; ?>
-                                        </a>
+                                        <a href="<?php echo $routevalue['url'];?>"><?php echo $lang[$config['lang']];?></a>
                                     </li>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
+                                <?php endif;?>
+                        <?php endforeach;?>
                         </ul>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+                    <?php endif;?>
+                </li>
+            <?php endforeach;?>
+        </ul>
         </div>
-
-
-
-        <div class="header">
-            <div class="logo" id="logo">
-                <h2 class="logo-bg">IBOS</h2>
-                <a href="javascript:;" id="db_map_ctrl" class="cbtn db-map-ctrl"></a>
-            </div>
-            <div class="hdbar clearfix" id="bar">
-                <form method="post" autocomplete="off" target="main" action="<?php echo $this->createUrl( 'default/search' ); ?>">
-                    <div class="dbsearch">
-                        <input type="text" name="keyword" placeholder="<?php echo $lang['Search']; ?>" x-webkit-speech="" speech="" class="input-small">
-                        <input type="hidden" name="formhash" value="<?php echo FORMHASH; ?>" />
-                    </div>
-                </form>
-                <div class="user-info pull-right">
-                    <span class="user-name">
-                        <a href="<?php echo Ibos::app()->user->space_url; ?>" target="_blank"><img width="30" height="30" class="radius msep" src="<?php echo Ibos::app()->user->avatar_middle; ?>" title="<?php echo Ibos::app()->user->realname; ?>"></a>
-                        <strong><?php echo Ibos::app()->user->realname; ?></strong>
-                    </span>
-                    <a href="<?php echo Ibos::app()->urlManager->createUrl( '/' ); ?>" target="_blank" class="msep cbtn o-homepage" title="<?php echo Ibos::lang( 'Return to home page' ); ?>"></a>
-                    <a href="<?php echo $this->createUrl( 'default/logout', array( 'formhash' => FORMHASH ) ); ?>" class="cbtn o-logout" title="<?php echo $lang['Logout']; ?>"></a>
-                </div>
-            </div>
-        </div>
-
-        <div class="mainer" id="mainer">
-            <div class="aside" id="aside">
-                <div class="main-nav">
-                    <ul id="main_nav">
-                        <?php $i = 0; ?>
-                        <?php foreach ( $cateConfig as $cate => $config ): ?>
-                            <li <?php if ( empty( $i ) ): ?>class="active"<?php
-                            endif;
-                            $i++;
-                            ?>>
-                                <a href="<?php echo $config['url']; ?>" target="main" data-href="#db_<?php echo $config['id']; ?>_list" id="db_<?php echo $config['id']; ?>" class="db-<?php echo $config['id']; ?>"><?php echo $lang[$config['lang']]; ?></a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-                <div class="sub-nav" id="sub_nav" style="overflow: auto">
-                    <?php $i = $j = 1; ?>
-                    <?php foreach ( $routes as $cate => $routeA ): ?>
-                        <ul id="db_<?php echo $cateConfig[$cate]['id']; ?>_list" <?php if ( $j !== 1 ): ?>style="display:none;"<?php endif; ?>>
-                            <?php foreach ( $routeA as $route => $config ): ?>
-                                <?php if ( $config['config']['isShow'] ): ?>
-                                    <li <?php if ( $i === 1 ): ?>class="active"<?php endif; ?>>
-                                        <?php $i++; ?>
-                                        <a href="<?php echo $config['url']; ?>" target="main" title="<?php echo $lang[$config['config']['lang']]; ?>">
-                                            <?php echo $lang[$config['config']['lang']]; ?>
-                                        </a>
-                                        <?php if ( $route == 'module/manager' && !empty( $moduleMenu ) ): ?>
-                                        <li class="active">
-                                            <ul class="sub-sec-nav">
-                                                <?php foreach ( $moduleMenu as $id => $menu ): ?>
-                                                    <li>
-                                                        <a href="<?php echo Ibos::app()->urlManager->createUrl( $menu['m'] . '/' . $menu['c'] . '/' . $menu['a'] ); ?>" target="main" title="<?php echo $menu['name']; ?>">
-                                                            <?php echo $menu['name']; ?>
-                                                        </a>
-                                                    </li>
-                                                <?php endforeach; ?>
-                                            </ul>
-                                        </li>
-                                    <?php endif; ?>
-                                    </li>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        </ul>
-                        <?php $j++; ?>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <div class="mc" id="mc">
-                <iframe src="<?php echo $def; ?>" width="100%" height="100%" frameborder="0" name="main" id="main"></iframe>
-            </div>
-        </div>
-
-        <!-- </div> -->
         <!-- load js -->
         <script src="<?php echo STATICURL; ?>/js/src/core.js?<?php echo VERHASH; ?>"></script>
         <script src="<?php echo STATICURL; ?>/js/lib/artDialog/artDialog.min.js?<?php echo VERHASH; ?>"></script>
